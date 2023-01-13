eb406bf962dea204ded4e7d50f4a1c72dfd35193
felixpro <42367473+felixpro@users.noreply.github.com>
2023-01-13 16:52:48 -0400

    Added new cypress config

---
+++ export.bash
@@ -0,0 +1,21 @@
+
+# IMPORT
+
+BASE_URL=https://xray.cloud.getxray.app
+PROJECT=XP
+ 
+zip -r features.zip cypress/integration/ -i \*.feature
+ 
+token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" https://xray.cloud.getxray.app/api/v2/authenticate| tr -d '"')
+curl -H "Content-Type: multipart/form-data" -H "Authorization: Bearer $token"  -F "file=@features.zip" "$BASE_URL/api/v2/import/feature?projectKey=$PROJECT"
+
+# EXPORT 
+
+
+
+BASE_URL=https://xray.cloud.getxray.app
+token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" $BASE_URL/api/v2/authenticate| tr -d '"')
+curl -H "Content-Type: application/json" -X GET -H "Authorization: Bearer $token" "$BASE_URL/api/v2/export/cucumber?keys=CALC-632;CALC-633" -o features.zip
+ 
+rm -rf features/*.feature
+unzip -o features.zip -d features
