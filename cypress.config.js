9ced986e1b2f4122b6d40b0df6f887f25433b234
felixpro <42367473+felixpro@users.noreply.github.com>
2023-01-13 13:16:58 -0400

    setup cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,55 @@
 const { defineConfig } = require("cypress");
+const webpack = require("@cypress/webpack-preprocessor");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    webpack({
+      webpackOptions: {
+        resolve: {
+          extensions: [".ts", ".js"],
+        },
+        module: {
+          rules: [
+            {
+              test: /\.feature$/,
+              use: [
+                {
+                  loader: "@badeball/cypress-cucumber-preprocessor/webpack",
+                  options: config,
+                },
+              ],
+            },
+          ],
+        },
+      },
+    })
+  );
+
+  // Useful trick for debugging: cy.task('log', 'soem text')
+  on("task", {
+    log(...params) {
+      console.log(...params);
+
+      return null;
+    },
+  });
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      numTestsKeptInMemory: 0;
-      video: false;
-    },
+    setupNodeEvents,
+    baseUrl:
+      "https://www.npmjs.com/package/@badeball/cypress-cucumber-preprocessor",
+    specPattern: "./cypress/integration/**/*.feature",
+    screenshotOnRunFailure: true,
+    experimentalStudio: true,
   },
 });
