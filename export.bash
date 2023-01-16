6b3d29c4c7b5e587d45858c9f0e50c954a443429
felixpro <42367473+felixpro@users.noreply.github.com>
2023-01-16 10:44:39 -0400

    Fix for export command

--- export.bash
+++ export.bash
@@ -14,7 +14,7 @@ curl -H "Content-Type: multipart/form-data" -H "Authorization: Bearer $token"  -
 
 
 BASE_URL=https://xray.cloud.getxray.app
-token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" $BASE_URL/api/v2/authenticate| tr -d '"')
+token=$(curl -H "Content-Type: application/json" -X POST --data '{"client_id": "B6539A608CA043E687A53283D6ED3CAF", "client_secret": "aa4c750d4565f125e48362f7386b60c0e7cbf2616db60b6199509db076656656"}' $BASE_URL/api/v2/authenticate| tr -d '"')
 curl -H "Content-Type: application/json" -X GET -H "Authorization: Bearer $token" "$BASE_URL/api/v2/export/cucumber?keys=XP-13" -o features.zip
  
 rm -rf features/*.feature
